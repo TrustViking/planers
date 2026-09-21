fdc951697060ea13fa54bbc351f53c341f3d226b
Artur <5688633485@my.lerna.md>
2026-09-21 16:26:57 +0300

    planer 0.1.9: сборка local

--- app/version.py
+++ app/version.py
@@ -10,7 +10,7 @@ import sys
 from pathlib import Path
 from typing import Final
 
-APP_VERSION: Final[str] = "0.1.8"
+APP_VERSION: Final[str] = "0.1.9"
 
 BUMP_FLAG: Final[str] = "--bump"
 VERSION_FORMAT: Final[re.Pattern[str]] = re.compile(r"^(\d+)\.(\d+)\.(\d+)$")
